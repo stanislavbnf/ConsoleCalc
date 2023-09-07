3e3fa55cb2c408c92006b746ca59374e97a0eaff
sbelialov <sergeibelialov@gmail.com>
2023-09-06 20:18:30 -0500

    Base

---
+++ ConsoleCalc/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Калькулятор:");
+            Console.WriteLine("1. Сложение");
+            Console.WriteLine("2. Вычитание");
+            Console.WriteLine("3. Умножение");
+            Console.WriteLine("4. Деление");
+            Console.WriteLine("5. Выход");
+
+            Console.Write("Выберите операцию (1/2/3/4/5): ");
+            
+            // Код пишем тут
+            // Не забываем про break;
+            // Не забываем про деление на ноль
+            // Не забываем про некорректный ввод (например, буквы)
+
+            //Console.Write("Введите первое число: ");
+
+            //Console.Write("Введите второе число: ");
+
+        }
+    }
+}
